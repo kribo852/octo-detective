b5a8c4902fc8c1dc8096fc86dc4486cf24a95308
kribo852 <kristoffer.a.borg@gmail.com>
2026-03-22 12:08:57 +0100

    Fixed some linting for some files, and fixed some texts

--- levels/map1.lua
+++ levels/map1.lua
@@ -7,27 +7,27 @@ set_detective {
 set_police_car {
 	position = {x = -1, y = 35}
 }
-make_clue{
+make_clue {
 	name = "ring",
 	type = "object",
 	is_discovered = false,
 	carried = true,
 	image = "ring.png",
-	description = "A ring found in the woods, maybe connected to the crime, I should get it analyzed for fingerprints",
+	description = "A ring found in the woods, maybe connected to the crime, I should get it analyzed for fingerprints.",
 	depends_on = none(),
 	discovery_positions = { {5, 5} }
 }
-make_clue{
+make_clue {
 	name = "victim",
 	type = "body",
 	is_discovered = false,
 	carried = false,
 	image = "dead_body.png",
-	description = "How gruesome, the body of a young woman. I must bring her justice and arrest her murderer",
+	description = "How gruesome, the body of a young woman. I must bring her justice and arrest her murderer.",
 	depends_on = none(),
 	discovery_positions = { {15, 15} }
 }
-make_clue{
+make_clue {
 	name = "wallet",
 	type = "object",
 	is_discovered = false,
@@ -37,23 +37,23 @@ make_clue{
 	depends_on = one("victim"),
 	discovery_positions = { {25, 0} }
 }
-make_clue{
+make_clue {
 	name="suspect",
 	type="person",
 	is_discovered = false,
 	carried = false,
 	image = "suspect.png",
-	description = "A picture of the perpetrator emerges, who is the the victims husband? \n I should go back to the police car and make a call to the station",
+	description = "A picture of the perpetrator emerges, who is the the victims husband?\nI should go back to the police car and make a call to the station.",
 	depends_on = all("wallet", "ring"),
 	discovery_wait = 10 --in seconds
 }
-make_clue{
+make_clue {
 	name = "Dan Wright",
 	type = "Person",
 	is_discovered = false,
-	carried = true, 
+	carried = true,
 	image = "suspect.png",
-	description = "Dan Wright is the husband of the victim Isabella, says the registers at the police station",
+	description = "Dan Wright is the husband of the victim Isabella, says the registers at the police station.",
 	depends_on = one("suspect"),
 	display_on_ground_image = "call_police_station.png",
 	discovery_positions = { {-2, 35},{0, 35},{-1, 34},{-1, 36} }
--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ function love.load()
 	menu = require "game_states.menu"
 	ingame = require "game_states.ingame.ingame"
 
-	init_game_state_transitions()	
+	init_game_state_transitions()
 end
 
 function init_game_state_transitions()
@@ -32,29 +32,28 @@ function init_game_state_transitions()
 	game_state_engine.init("menu") --start in a menu state
 end
 
-function love.update(delta_time) 
-	game_state_engine.update(delta_time, 
-		{ 
-		  transition_game_state=transition_to_game_state, 
+function love.update(delta_time)
+	game_state_engine.update(delta_time,
+		{
+		  transition_game_state=transition_to_game_state,
 		  transition_to_menu_state=transition_to_menu_state,
 		  to_menu_state=to_menu_state
 		}
 	)
 end
 
-
 function love.draw()
 	game_state_engine.draw({})
 end
 
 function transition_to_game_state()
- 	game_state_engine.set_next_state("ingame") 
-end	
+	game_state_engine.set_next_state("ingame")
+end
 
 function transition_to_menu_state()
- 	game_state_engine.set_next_state("transition_to_menu") 
-end	
+	game_state_engine.set_next_state("transition_to_menu")
+end
 
 function to_menu_state()
- 	game_state_engine.set_next_state("menu") 
-end	
+	game_state_engine.set_next_state("menu")
+end
