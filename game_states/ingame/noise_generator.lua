714b4bba7e47d0474eb623ed74ea244ad9c9256f
kribo852 <kristoffer.a.borg@gmail.com>
2026-07-29 17:22:21 +0200

    Modified the algorithm for getting random numbers for noisy grass

--- game_states/ingame/noise_generator.lua
+++ game_states/ingame/noise_generator.lua
@@ -1,22 +1,32 @@
+local autoexpand_metatable = {
+	__index = function(tab, key)
+    	tab[key] = {}
+    	return tab[key]
+	end
+}
+
 local noise_generator = {
 
 }
 
 function noise_generator.new_random_func(scale)
-		local cache_l1 = {}
-		local cache_l2 = {}
+		local cache_randoms = {}
+		local cache_smooth_summed_randoms = {}
+
+		setmetatable(cache_randoms, autoexpand_metatable)
+		setmetatable(cache_smooth_summed_randoms, autoexpand_metatable)
 
 		local random_for_position = function(x, y)
-			if not cache_l1[x.."#"..y] then
-				cache_l1[x.."#"..y] = love.math.random() -- this is just to store the random values, so that repeated lookup gives the same value again
+			if not cache_randoms[x][y] then
+				cache_randoms[x][y] = love.math.random() -- this is just to store the random values, so that repeated lookup gives the same value again
 			end
-			return cache_l1[x.."#"..y]
+			return cache_randoms[x][y]
 		end
 
 		local smooth_random = function (x, y)
 			scale = scale or 2
 
-			if not cache_l2[x.."#"..y] then
+			if not cache_smooth_summed_randoms[x][y] then
 				local sum = 0
 				local no_of_entries = (2*scale+1)^2
 
@@ -25,9 +35,9 @@ function noise_generator.new_random_func(scale)
 						sum = sum + random_for_position(x+i, y+j)
 					end
 				end
-				cache_l2[x.."#"..y] = sum / no_of_entries
+				cache_smooth_summed_randoms[x][y] = sum / no_of_entries
 			end
-			return cache_l2[x.."#"..y]
+			return cache_smooth_summed_randoms[x][y]
 		end
 
 	return {
